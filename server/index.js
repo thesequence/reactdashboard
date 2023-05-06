e22b29f40c237fa30b477b22d41c5a32d1c979ae
Denis Makarov <qamakarov@gmail.com>
2023-05-06 18:15:49 +0200

    add server and react app

---
+++ server/index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import overviewRoutes from "./routes/overview.js";
+import stocksRoutes from "./routes/stocks.js";
+import workforceRoutes from "./routes/workforce.js";
+import accountingRoutes from "./routes/accounting.js";
+import generalRoutes from "./routes/general.js";
+
+/* CONFIGURATION */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES */
+app.use("/overview", overviewRoutes);
+app.use("/stocks", stocksRoutes);
+app.use("/workforce", workforceRoutes);
+app.use("/accounting", accountingRoutes);
+app.use("/general", generalRoutes);
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+  }).catch((error ) => console.log(`${error} did not connect`));
